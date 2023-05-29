0af196fcf11ff8d06d82a4b3f1d05d8d397ccc1d
UPBWinston <B00138942@mytudublin.ie>
2023-05-29 09:52:49 +0100

    Testing postgres connection via food api

--- pages/api/food.js
+++ pages/api/food.js
@@ -1,19 +1,29 @@
-import { setResponseStatusBasedOnError } from "./utils"
-import { sql } from "@vercel/postgres";
+import { db } from '@vercel/postgres';
 
 
 async function addNewFood(req, res) {
-    const connection = getDatabaseConnection();
-    await sql`insert into test.food(name, calories, amount, unit, color) 
-            values ("${req.body.name}", "${req.body.calories}", "${req.body.amount}", "${req.body.unit}", "${req.body.color}")
-            on duplicate key update calories="${req.body.calories}", amount="${req.body.amount}", unit="${req.body.unit}", color="${req.body.color}"`;
-    res.status(200).json("ok");
+    const client = await db.connect();
+
+    try {
+        await client.sql`insert into test.food(name, calories, amount, unit, color) 
+        values ("${req.body.name}", "${req.body.calories}", "${req.body.amount}", "${req.body.unit}", "${req.body.color}")
+        on duplicate key update calories="${req.body.calories}", amount="${req.body.amount}", unit="${req.body.unit}", color="${req.body.color}"`;
+        res.status(200).json("ok");
+    } catch (error) {
+        return response.status(500).json({ error });
+    }
+
+
 }
 
 async function getFood(req, res) {
-    const connection = getDatabaseConnection();
-    const {results} =  await sql`select * from test.food`;
-    req.status(200).json(results);
+    const client = await db.connect();
+    try {
+        const results = await sql`select * from test.food`;
+        req.status(200).json(results);
+    } catch (error) {
+        return response.status(500).json({ error });
+    }
 }
 
 export default async function handler(req, res) {
