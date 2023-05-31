b7ab13d909e18977e7fdbbdfea9f9574f1eae9d9
UPBWinston <B00138942@mytudublin.ie>
2023-05-31 20:30:40 +0100

    Fixed issue with unit type selection

--- shared/components/Home/WorkoutModal.js
+++ shared/components/Home/WorkoutModal.js
@@ -44,7 +44,7 @@ export const WorkoutModal = ({workout, button}) => {
             color: modalColor,
             date: workout.date,
             type: selectedTypeValue,
-            unit: selectedUnit,
+            unit: selectedUnitValue,
         });
         refreshPage();
     }
